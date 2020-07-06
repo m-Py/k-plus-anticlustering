f9bf6bf633428a8eb626fd44e11a07f986676d75
Martin Papenberg <martin.papenberg@hhu.de>
2020-07-06 16:33:36 +0200

    implemented improved exchange method into anticlust

--- improved_objective.R
+++ improved_objective.R
@@ -5,93 +5,45 @@ squared_from_mean <- function(data) {
   apply(data, 2, function(x) (x - mean(x))^2)
 }
 
-improved_exchange_ <- function(nrep, x, K, objective, obj_function, 
-                               categories, preclustering) {
-  x <- as.matrix(x)
-  N <- nrow(x)
-  clusters <- anticlustering(
-    x, K, objective = objective, categories = categories
-  )
-  new_obj <- obj_function(x, clusters)
-  old_obj <- -1
-  while (new_obj > old_obj) {
-    clusters <- anticlustering(
-      x, K, objective = objective
-    )
-    old_obj <- new_obj
-    new_obj <- obj_function(x, clusters)
-  }
-  clusters
-}
 
-improved_exchange <- function(x, K, objective, obj_function, 
-                              categories = NULL, preclustering = FALSE, 
-                              nrep = 10) {
-  candidate_solutions <- lapply(
-    1:nrep, 
-    improved_exchange_,
-    x = x, 
-    K = K, 
-    objective = objective, 
-    obj_function = obj_function,
-    categories = categories, 
-    preclustering = preclustering
-  )
-  obj_function_ <- function(clusters, x) {
-    obj_function(x, clusters)
-  }
-  objs <- lapply(
-    candidate_solutions,
-    obj_function_,
-    x = x
-  )
-  candidate_solutions[[which.max(objs)]]
-}
+N <- 60
+M <- 1
+K <- 3
+features <- matrix(rnorm(N * M), ncol = M)
+#features <- schaper2019[, 3:6]
 
+nrep <- 30
 
-N <- 500
-M <- 2
-K <- 10 
-features <- matrix(rchisq(N * M, 2), ncol = M)
-#features <- scale(schaper2019[, 3:6])
-
-nrep <- 20
-preclustering <- FALSE
-
-ac_kmeans <- improved_exchange(
+ac_kmeans <- anticlustering(
   features,
   K = K,
   objective = "variance",
-  obj_function = variance_objective, 
-  preclustering = preclustering,
-  nrep = nrep
+  method = "local-maximum",
+  repetitions = nrep
 )
 
-ac_distance <- improved_exchange(
+ac_distance <- anticlustering(
   features,
   K = K,
   objective = "distance",
-  obj_function = variance_objective, 
-  preclustering = preclustering,
-  nrep = nrep
+  method = "local-maximum",
+  repetitions = nrep,
 )
 
-ac_kmeans_var_only <- improved_exchange(
+ac_kmeans_var_only <- anticlustering(
   squared_from_mean(features),
   K = K,
   objective = "variance",
-  obj_function = variance_objective,
-  preclustering = preclustering,
-  nrep = nrep
+  method = "local-maximum",
+  repetitions = nrep
 )
 
-ac_kmeans_var <- improved_exchange(
-  cbind(features, squared_from_mean(features)),
+ac_kmeans_var <- anticlustering(
+  scale(cbind(features, squared_from_mean(features))),
   K = K,
   objective = "variance",
-  obj_function = variance_objective,
-  preclustering = preclustering,
-  nrep = nrep
+  method = "local-maximum",
+  repetitions = nrep
 )
 
 cat("Standard K-Means \n")
@@ -100,8 +52,5 @@ cat("K-Means + Squared Diff\n")
 print(mean_sd_tab(features, ac_kmeans_var, return_diff = TRUE))
 cat("Anticluster Editing \n")
 print(mean_sd_tab(features, ac_distance, return_diff = TRUE))
-#cat("Only Squared Diff\n")
-#print(mean_sd_tab(features, ac_kmeans_var_only, return_diff = TRUE))
-
-
-
+cat("Only Squared Diff\n")
+print(mean_sd_tab(features, ac_kmeans_var_only, return_diff = TRUE))
