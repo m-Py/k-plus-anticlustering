7fe434f72ddaf0768e74e3b2708bfdb649b40a57
Martin Papenberg <martin.papenberg@hhu.de>
2020-07-14 17:17:35 +0200

    more efficient computation of pareto set

--- pareto_front.R
+++ pareto_front.R
@@ -5,29 +5,21 @@
 library(anticlust)
 source("misc-functions.R")
 
-N <- 14
-K <- 2
+N <- 12
+K <- 3
 M <- 2
 all_partitions <- generate_partitions(N, K)
 features <- matrix(rnorm(N * M), ncol = M)
 
-obj_fun <- function(partitions, features) {
-  variance_objective(features, partitions)
-}
-
-squared_from_mean <- function(data) {
-  apply(data, 2, function(x) (x - mean(x))^2)
-}
-
 all_objectives_mean <- sapply(
   all_partitions,
-  FUN = obj_fun,
+  FUN = var_objective,
   features = features
 )
 
 all_objectives_var <- sapply(
   all_partitions,
-  FUN = obj_fun,
+  FUN = var_objective,
   features = squared_from_mean(features)
 )
 
@@ -38,11 +30,10 @@ df <- data.frame(
 
 # Compute Pareto efficient set!
 
-plot(df, col = "darkgrey", las = 1, pch = 4, cex = 0.8)
-
 # For a data frame (rows = partitions, columns = objectives), determine which 
 # elements are part of the Pareto efficient set. Output is a logical vector,
 # illustrating for each row if it is part of the Pareto efficient set.
+
 pareto_set <- function(df) {
   is_dominated <- rep(FALSE, nrow(df)) 
   for (i in 1:nrow(df)) {
@@ -57,8 +48,66 @@ pareto_set <- function(df) {
   !is_dominated
 }
 
-efficient_set <- pareto_set(df)
+# Try some speed optimization
+pareto_set2 <- function(df) {
+  df <- data.frame(df)
+  ncrits <- ncol(df)
+  df$ID <- 1:nrow(df)
+  df <- df[order(rowSums(df[, 1:ncrits]), decreasing = TRUE), ]
+  
+  is_dominated <- rep(FALSE, nrow(df)) 
+  for (i in nrow(df):1) {
+    for (j in 1:nrow(df)) {
+      j_dominates_i <- all(df[i, 1:ncrits] <= df[j, 1:ncrits]) && 
+                       any(df[i, 1:ncrits] < df[j, 1:ncrits])
+      if (j_dominates_i) {
+        is_dominated[df$ID[i]] <- TRUE
+        break
+      }
+    }
+  }
+  !is_dominated
+}
+
+#start <- Sys.time()
+#efficient_set2 <- pareto_set2(df)
+#Sys.time() - start 
+
+#start <- Sys.time()
+#efficient_set <- pareto_set(df)
+#Sys.time() - start 
+#all.equal(efficient_set, efficient_set2)
+
+
+start <- Sys.time()
+efficient_set <- pareto_set2(df)
+Sys.time() - start 
+
+sum(efficient_set)
+
 plot(df, col = "darkgrey", las = 1, pch = 4, cex = 0.8)
 points(df[efficient_set, ], col = "red", cex = 1.2, pch = 19)
+
+
+
+# Do plot for difference in means / vars
+
+df2 <- data.frame(
+  diff_mean = sapply(
+      all_partitions,
+      sum_group_diff_min_max,
+      features = features,
+      fun = mean
+  ),
+  diff_var = sapply(
+      all_partitions,
+      sum_group_diff_min_max,
+      features = features,
+      fun = var
+  )
+)
+
+plot(df2, col = "darkgrey", las = 1, pch = 4, cex = 0.8)
+points(df2[efficient_set, ], col = "red", cex = 1.2, pch = 19)
 
 
