e93be1a8c340e212eeacdc6b9c4d987f1e1649a1
Martin Papenberg <martin.papenberg@hhu.de>
2022-02-25 14:48:06 +0100

    general function for any higher order moments

--- scripts/Example-4-OASIS-Skew_Kurtosis.R
+++ scripts/Example-4-OASIS-Skew_Kurtosis.R
@@ -5,26 +5,17 @@ library(DescTools) # for computing skew and kurtosis
 oasis <- read.csv("https://raw.githubusercontent.com/aenneb/OASIS-beauty/master/means_per_image.csv")
 
 # function to compute features for variance
-squared_from_mean <- function(data) {
-  apply(data, 2, function(x) (x - mean(x))^2)
-}
-
-# function to compute features for skewness
-third_moment_features <- function(data) {
-  apply(data, 2, function(x) ((x - mean(x))/sd(x))^3 )
-}
-
-# function to compute features for skewness
-fourth_moment_features <- function(data) {
-  apply(data, 2, function(x) ((x - mean(x))/sd(x))^4 )
+# moment = 2 -> variance; 3 = skew; 4 = kurtosis
+moment_features <- function(data, moment = 2) {
+  apply(data, 2, function(x) (x - mean(x))^moment)
 }
 
 features <- subset(oasis, select = c(beauty_mean, Valence_mean, Arousal_mean))
-extended <- cbind(features, squared_from_mean(features))
+extended <- cbind(features, moment_features(features))
 extended_skew_kurtosis <- cbind(
   extended, 
-  third_moment_features(features),
-  fourth_moment_features(features)
+  moment_features(features, 3),
+  moment_features(features, 4)
 )
 
 # standard k-plus anticlustering
@@ -51,4 +42,3 @@ by(features, anticlusters_cov, function(x) round(apply(x, 2, Kurt), 2))
 mean_sd_tab(features, anticlusters_skew_kurtosis)
 mean_sd_tab(features, anticlusters_cov)
 
-table(oasis$ac, oasis$Category)
