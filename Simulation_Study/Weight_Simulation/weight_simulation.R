e4b640416adddda57c8d964cff38007fe2f61388
Martin Papenberg <martin.papenberg@hhu.de>
2023-04-19 15:50:27 +0200

    print time for all data sets

--- Simulation_Study/Weight_Simulation/weight_simulation.R
+++ Simulation_Study/Weight_Simulation/weight_simulation.R
@@ -3,19 +3,19 @@
 
 source("./Simulation_Study/Weight_Simulation/functions_weight_simulation.R")
 
-nsim <- 10
+nsim <- 30
 results <- list()
+start <- Sys.time()
 for (i in 1:nsim) {
   K <- sample(2:4, size = 1)
   N <- sample((10:100)[10:100 %% K == 0], size = 1)
   M <- sample(1:5, size = 1)
   message("Starting to work on N = ", N, ", K = ", K, ", M = ", M, ".")
   dat <- matrix(rnorm(N * M), ncol = M)
-  start <- Sys.time()
   results[[i]] <- weight_sim(dat, K = K)
-  print(Sys.time() - start)
   results[[i]] <- cbind(results[[i]], rowMeans(results[[i]]))
   colnames(results[[i]]) <- c("SSE", "SSE_Var", "SSE_Skew", "SSE_Kurtosis", "Average")
 }
+print(Sys.time() - start)
 
 save(results, file = paste0("./Simulation_Study/Weight_Simulation/Results Sim 1 ", date(), ".Rdata"))
