be3c8d7cc42a7a414bfde8b8bea4eab4a6d3e913
Martin Papenberg <martin.papenberg@hhu.de>
2023-04-20 16:22:14 +0200

    maybe chisquared test on all values can be used

--- Simulation_1/2_analysis.R
+++ Simulation_1/2_analysis.R
@@ -10,9 +10,14 @@ for (file in files) {
 round(Reduce("+", results) / length(results) * 100, 2)
 
 # how often was unweighted standardization approach best:
-unstd_best <- sapply(results, function(x) names(which.max(x[, 5]))) == "unweighted_std"
+names_best_method <- sapply(results, function(x) names(which.max(x[, 5])))
+unstd_best <- names_best_method == "unweighted_std"
 sum(unstd_best)
 mean(unstd_best)
 
 # Significance test
+frequency_table <- c(table(names_best_method), rep(0, 6 - length(table(names_best_method))))
+chisq.test(frequency_table)
+
+# Or simply:
 prop.test(x = sum(unstd_best), n = length(unstd_best), p = 1/6)
