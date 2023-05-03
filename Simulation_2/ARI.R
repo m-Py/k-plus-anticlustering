c966eb20c534d4e7d75e560caa91b0fc75b33fbd
Martin Papenberg <martin.papenberg@hhu.de>
2023-05-03 16:45:24 +0200

    this was not needed

--- Simulation_2/ARI.R
+++ Simulation_2/ARI.R
@@ -24,14 +24,11 @@ for (K in Ks) {
   data <- read.csv2(paste0("results-K", K, "-solutions.csv"), stringsAsFactors = FALSE)
   
   # ensure that the order of methods is good
-  n_methods <- length(unique(data$method))
-  apply(matrix(data$method, ncol = n_methods, byrow = TRUE), 2, function(x) all(x == x[1]))
   all_solutions <- lapply(data$result, anticlusters_from_string)
-  names(all_solutions) <- paste0(rep_len(data$method[1:n_methods], length(all_solutions)), "-", data$ID)
+  names(all_solutions) <- paste0(data$method, "-", data$ID)
   
   # exclude correlation method
   all_solutions <- all_solutions[!grepl("correlation", names(all_solutions))]
-  
   
   all_aris <- vector(mode = "list", length = length(unique(data$ID)))
   names(all_aris) <- unique(data$ID)
