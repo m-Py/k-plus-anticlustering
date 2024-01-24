0f75fa3f601f68e0c3776fbdc953640f7d6bdc6b
Martin Papenberg <martin.papenberg@hhu.de>
2024-01-24 14:05:57 +0100

    may use separate dispersion distances (should be factor in simulation)

--- Simulation_2/BILS_METHODS.R
+++ Simulation_2/BILS_METHODS.R
@@ -1,29 +1,32 @@
 ## Functions for simulation
 
-BILS_VANILLA <- function(data, K, RUNS_MBPI) {
+BILS_VANILLA <- function(data, K, RUNS_MBPI, dispersion_distances = dist(data)) {
   PARTITIONS <- bicriterion_anticlustering(
     data, 
     K = K, 
-    R = c(RUNS_MBPI, 0)
+    R = c(RUNS_MBPI, 0),
+    dispersion_distances = dispersion_distances
   )
-  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = data)), ]
+  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = dispersion_distances)), ]
 }
 
-BILS_E_1 <- function(data, init_partition, RUNS_MBPI) {
+BILS_E_1 <- function(data, init_partition, RUNS_MBPI, dispersion_distances = dist(data)) {
   PARTITIONS <- bicriterion_anticlustering(
     data, 
     K = init_partition, 
-    R = c(RUNS_MBPI, 0)
+    R = c(RUNS_MBPI, 0),
+    dispersion_distances = dispersion_distances
   )
-  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = data)), ]
+  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = dispersion_distances)), ]
 }
 
-BILS_E_ALL <- function(data, init_partitions) {
+BILS_E_ALL <- function(data, init_partitions, dispersion_distances = dist(data)) {
   PARTITIONS <- bicriterion_anticlustering(
     data, 
     K = init_partitions[1, ], # mostly irrelevant if `init_partitions` is passed
     R = c(nrow(init_partitions), 0),
-    init_partitions = init_partitions
+    init_partitions = init_partitions,
+    dispersion_distances = dispersion_distances
   )
-  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = data)), ]
+  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = dispersion_distances)), ]
 }
