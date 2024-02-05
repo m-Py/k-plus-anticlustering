4c427802a81efeeec469d552e0b7d0f95e42ffe8
Martin Papenberg <martin.papenberg@hhu.de>
2024-02-05 13:54:23 +0100

    plot pareto set

--- Application/schaper2019.R
+++ Application/schaper2019.R
@@ -48,8 +48,8 @@ schaper2019[t(positions_by_value(lt, value = sort(lt)[5])), ]
 # Definition of Levenshtein normalization as done above seems to be reasonable
 
 nparts <- 1000
-max_disp <- optimal_dispersion(lt, K = 3)
-GLOBAL_OPTIMUM_DISPERSION <- max_disp$dispersion
+opt <- optimal_dispersion(lt, K = 3)
+GLOBAL_OPTIMUM_DISPERSION <- opt$dispersion
 GLOBAL_OPTIMUM_DISPERSION
 
 # what's the worst that happend with regard to pairwise similarity (for example)
@@ -63,81 +63,125 @@ schaper2019[t(positions_by_value(lt, value = GLOBAL_OPTIMUM_DISPERSION)), ]
 features <- schaper2019[, 3:6]
 # Use k-plus and categorical variable (then the categorical variable is not included as
 # hard constraint, may facilitate finding solution if we already have the cannot-link restrictions)
-kplus_features <- scale(cbind(features, anticlust:::squared_from_mean(features), as.numeric(schaper2019$room == "bathroom")))
+kplus_features <- cbind(kplus_moment_variables(features, 2), categories_to_binary(schaper2019$room))
+kplus_distances <- as.matrix(dist(kplus_features)^2)
 
-disp_obj <- function(clusters, x) {
-  dispersion_objective(x, clusters)
-}
-var_obj <- function(clusters, x) {
-  variance_objective(x, clusters)
-}
-
-nparts <- 10000
+nparts <- 5000
 group_sizes <- c(32, 32, 32)
-max_disp <- optimal_dispersion(lt, K = group_sizes, npartitions = nparts)
-nrow(max_disp$groups)
+opt <- optimal_dispersion(lt, K = group_sizes, npartitions = nparts)
+nrow(opt$groups)
 # all(apply(max_disp$groups, 1, table) == apply(max_disp$groups, 1, table)[,1])
+sum(duplicated(opt$groups))
+
 
 start <- Sys.time()
 BILS_E_ALL <- bicriterion_anticlustering(
-  as.matrix(dist(kplus_features)^2), 
-  K = max_disp$groups[1,], 
+  kplus_distances, 
+  K = opt$groups[1,], 
   R = c(nparts, 1), 
   dispersion_distances = lt, 
-  init_partitions = max_disp$groups,
+  init_partitions = opt$groups,
   average_diversity = TRUE
 )
 Sys.time() - start 
-BILS_E_ALL <- BILS_E_ALL[which.max(apply(BILS_E_ALL, 1, disp_obj, x = lt)), ]
+
+ALL_CRITERIA <- list()
+ALL_CRITERIA$DISPERSIONS <- list()
+ALL_CRITERIA$DIVERSITIES <- list()
+
+ALL_CRITERIA$DISPERSIONS$BILS_E_ALL <- apply(BILS_E_ALL, 1, dispersion_objective, x = lt)
+ALL_CRITERIA$DIVERSITIES$BILS_E_ALL <- apply(BILS_E_ALL, 1, diversity_objective, x = kplus_distances)
 
 start <- Sys.time()
 BILS_E_1 <- bicriterion_anticlustering(
-  as.matrix(dist(kplus_features)^2), 
-  K = max_disp$groups[1,], 
+  kplus_distances, 
+  K = opt$groups[1,], 
   R = c(nparts, 1), 
   dispersion_distances = lt,
   average_diversity = TRUE
 )
 Sys.time() - start 
-BILS_E_1 <- BILS_E_1[which.max(apply(BILS_E_1, 1, disp_obj, x = lt)), ]
+
+ALL_CRITERIA$DISPERSIONS$BILS_E_1 <- apply(BILS_E_1, 1, dispersion_objective, x = lt)
+ALL_CRITERIA$DIVERSITIES$BILS_E_1 <- apply(BILS_E_1, 1, diversity_objective, x = kplus_distances)
+
 
 start <- Sys.time()
 BILS_VANILLA <- bicriterion_anticlustering(
-  as.matrix(dist(kplus_features)^2), 
+  kplus_distances, 
   K = group_sizes, 
   R = c(nparts, 1),
   average_diversity = TRUE, # not really "VANILLA"
   dispersion_distances = lt # not really "VANILLA"
 )
 Sys.time() - start 
-BILS_VANILLA <- BILS_VANILLA[which.max(apply(BILS_VANILLA, 1, disp_obj, x = lt)), ]
 
-start <- Sys.time()
-LCW_RESTRICTED <- cannot_link_anticlustering(
-  kplus_features, 
-  init_clusters = max_disp$groups, 
-  cannot_link = max_disp$edges, 
-  objective = "variance",
-  method = "local-maximum"
+ALL_CRITERIA$DISPERSIONS$BILS_VANILLA <- apply(BILS_VANILLA, 1, dispersion_objective, x = lt)
+ALL_CRITERIA$DIVERSITIES$BILS_VANILLA <- apply(BILS_VANILLA, 1, diversity_objective, x = kplus_distances)
+
+
+tmp_dists <- as.matrix(lt)
+tmp_dists[rbind(opt$edges, t(apply(opt$edges, 1, rev)))] <- -99999999999999 # lazy large number, can be computed exactly
+
+E_ALL_RESTRICTED <- bicriterion_anticlustering(
+  kplus_distances,
+  K = opt$groups[1,],
+  R = c(nparts, 1),
+  init_partitions = opt$groups,
+  dispersion_distances = tmp_dists
 )
-Sys.time() - start # about 4x as fast as BILS with new implementation (for 10000 repetitions)
+ALL_CRITERIA$DISPERSIONS$E_ALL_RESTRICTED <- apply(E_ALL_RESTRICTED, 1, dispersion_objective, x = lt)
+ALL_CRITERIA$DIVERSITIES$E_ALL_RESTRICTED <- apply(E_ALL_RESTRICTED, 1, diversity_objective, x = kplus_distances)
 
-dispersion_objective(lt, LCW_RESTRICTED)
-dispersion_objective(lt, BILS_VANILLA)
-dispersion_objective(lt, BILS_E_1) # sometimes not optimal for unequal-sized groups!? BUG!!
-dispersion_objective(lt, BILS_E_ALL)
-variance_objective(kplus_features, LCW_RESTRICTED) 
-variance_objective(kplus_features, BILS_VANILLA)
-variance_objective(kplus_features, BILS_E_1)
-variance_objective(kplus_features, BILS_E_ALL)
+# VANILLA FOUND OPTIMUM?
+max(ALL_CRITERIA$DISPERSIONS$BILS_VANILLA) == opt$dispersion
 
-## Problems for unequal-sized groups when using BILS-E
+## PLOT PARETO SETS
 
-# best found solution so far:
-# 854.8995 for 10000 repetitions using restricted LCW (so using forbidden edges may be useful; implement in BILS-E!)
-# 854.9021 for 10000+1 repetitions using BILS-E-ALL (not using forbidden edges!)
+plot(
+  ALL_CRITERIA$DISPERSIONS$E_ALL_RESTRICTED,
+  ALL_CRITERIA$DIVERSITIES$E_ALL_RESTRICTED,
+  col = "#ABCDEF",
+  cex = 1.2,
+  pch = 17,
+  ylim = c(min(unlist(ALL_CRITERIA$DIVERSITIES)), max(unlist(ALL_CRITERIA$DIVERSITIES))),
+  xlim = c(min(unlist(ALL_CRITERIA$DISPERSIONS)), max(unlist(ALL_CRITERIA$DISPERSIONS))),
+  main = "5000 iterations for MBPI",
+  ylab = "Diversity",
+  xlab = "Dispersion"
+)
 
-mean_sd_tab(schaper2019[,3:6], LCW_RESTRICTED)
-mean_sd_tab(schaper2019[,3:6], BILS_VANILLA)
-mean_sd_tab(schaper2019[,3:6], BILS_E_1)
-mean_sd_tab(schaper2019[,3:6], BILS_E_ALL)
+points(
+  sort(ALL_CRITERIA$DISPERSIONS$BILS_VANILLA),
+  ALL_CRITERIA$DIVERSITIES$BILS_VANILLA[order(ALL_CRITERIA$DISPERSIONS$BILS_VANILLA)],
+  col = "red",
+  pch = 19,
+  cex = .7,
+  type = "b"
+)
+
+points(
+  sort(ALL_CRITERIA$DISPERSIONS$BILS_E_ALL),
+  ALL_CRITERIA$DIVERSITIES$BILS_E_ALL[order(ALL_CRITERIA$DISPERSIONS$BILS_E_ALL)],
+  col = "orange",
+  pch = 25,
+  cex = 1,
+  bg = "orange",
+  type = "b"
+)
+
+points(
+  sort(ALL_CRITERIA$DISPERSIONS$BILS_E_1),
+  ALL_CRITERIA$DIVERSITIES$BILS_E_1[order(ALL_CRITERIA$DISPERSIONS$BILS_E_1)],
+  col = "green",
+  pch = 17,
+  type = "b",
+  cex = 1,
+  bg = "green"
+)
+
+legend("bottomleft", legend = c("VANILLA", "E_ALL_RESTRICTED", "E_ALL", "E_1"),
+       pch = c(19, 17, 25), col = c("red", "#ABCDEF", "orange", "green"),
+       pt.bg = c("red", "#ABCDEF", "orange", "green"), cex = 1.2)
+
+
