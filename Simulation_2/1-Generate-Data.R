03578a7e62efda4f72684e621650ae00fd3c14b3
Martin Papenberg <martin.papenberg@hhu.de>
2024-02-16 12:30:39 +0100

    removed old data sets

--- Simulation_2/1-Generate-Data.R
+++ Simulation_2/1-Generate-Data.R
@@ -5,15 +5,19 @@
 # First step in the simulation: Generate data
 source("0-functions-generate-data.R")
 
-sample_sizes <- 12 * 2:25 # 24 - 300
-nsets <- 10000
+sample_sizes <- 20:120 # 24 - 300
+nsets <- 1000
 
-N  <- sample(sample_sizes, replace = TRUE, size = nsets)
-M  <- sample(2:5, replace = TRUE, size = nsets)
-SD <- sample(c(1, 2, 3), replace = TRUE, size = nsets)
-r  <- sample(c(0, .1, .2, .3, .4, .5), replace = TRUE, size = nsets)
-
-# Store data sets as files
-for (i in 1:nsets) {
-  generate_data(N[i], M[i], SD[i], r[i], dir = "./datasets/")
+for (K in 2:7) {
+  Ns <- sample_sizes[sample_sizes %% K == 0]
+  N  <- sample(Ns, replace = TRUE, size = nsets)
+  M  <- sample(2:5, replace = TRUE, size = nsets)
+  SD <- sample(c(1, 2, 3), replace = TRUE, size = nsets)
+  # Store data sets as files
+  for (i in 1:nsets) {
+    generate_data(N[i], M[i], SD[i], dir = paste0("./datasets/K", K))
+  }
 }
+
+
+
