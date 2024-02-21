355e6475777f6f89aa6158413ca447450267a1b5
Martin Papenberg <martin.papenberg@hhu.de>
2024-02-21 13:16:59 +0100

    some cleanup

--- Simulation_2/Analysis_BILS.R
+++ Simulation_2/Analysis_BILS.R
@@ -1,7 +1,6 @@
 library(anticlust)
 library(tidyr)
 library(dplyr)
-library(BayesFactor)
 
 df <- read.csv("results_bils.csv", sep = ";")
 sum(!complete.cases(df)) # =)
@@ -55,9 +54,10 @@ df |>
     E_ALL = mean(DIV_E_ALL),
     E_ALL_RESTRICTED = mean(DIV_E_ALL_RESTRICTED),
     VANILLA = mean(DIV_VANILLA)
-  )
+  ) |>
+  as.data.frame()
 
-table(df$N_DUPLICATE_PARTITIONS > 0, df$K) # also check out results in dependence of duplicate partitions!
+table(df$N_DUPLICATE_PARTITIONS > 0, df$K) # also checkCP Conference Index in DBLP out results in dependence of duplicate partitions!
 
 df$MAXIMUM_RESTRICTION <- df$N_DUPLICATE_PARTITIONS == (df$RUNS_BILS_VANILLA - 1)
 
@@ -83,11 +83,7 @@ df |>
     E_ALL_RESTRICTED = mean(DIV_E_ALL_RESTRICTED)
   )
 
-
-
-
-
-tt <- df[df$VANILLA_FOUND_OPTIMUM, ]
+tt <- df[df$VANILLA_FOUND_OPTIMUM, ] # slight bias in favor of VANILLA, in particular for K = 6, K = 7
 
 t.test(tt$DIV_VANILLA, tt$DIV_E_1, paired = TRUE)
 t.test(tt$DIV_VANILLA, tt$DIV_E_ALL, paired = TRUE)
@@ -95,44 +91,13 @@ t.test(tt$DIV_VANILLA, tt$DIV_E_ALL_RESTRICTED, paired = TRUE)
 t.test(tt$DIV_E_1, tt$DIV_E_ALL, paired = TRUE)
 t.test(tt$DIV_E_1, tt$DIV_E_ALL_RESTRICTED, paired = TRUE)
 t.test(tt$DIV_E_ALL, tt$DIV_E_ALL_RESTRICTED, paired = TRUE)
-# VANILLA seems to best optimize the diversity, but does not always find the optimal dispersion. so this analysis is actually flawes because of systematic missing values... what is the most appropriate analysis? BILS-E with restrictions may even be better with regard to max diversity! (even in flawed analysis with dropouts)
 
 # Important take aways (preliminary)
 
 # - Diversity is lower if the dispersion is optimized on the basis of different data (that makes sense I guess)
 # - E_ALL is better than E_1
 # - VANILLA BILS has more difficulties finding the optimal diversity if the dispersion is optimized on the basis of a different data set
-# - The max dispersion problem can be solved in reasonable time using an open source solver (!) for rather large data sets (N = 300, K = 4) (current maximum solving time 200 seconds)
+# - The max dispersion problem can be solved in reasonable time using an open source solver (!) for rather large data sets (N = 300, K = 4; N = 120, K = 7)
 # - Restricted method is the best extension that ensures the maximum dispersion (This would indicate that using unicriterion LCW is just as effective as BILS)
 # - Interpretation: Best for optimizing diversity while maintaining the global optimum in dispersion: restricted version if there are many init partitions that have the optimal value in dispersion. Worst: Only pass 1 init partition. Vanilla is usually good, but does not necessarily find global optimum (this can be checked using the optimal_dispersion() function). If the global optimum is needed, use restricted version.
-# RESTRICTED METHOD IS NOT GOOD IF THE NUMBER OF UNIQUE PARTITIONS IS SMALL (shown in example application)
-
-
-
-lf <- pivot_longer(df, cols = starts_with(c("DIV", "DISP")))
-lf$Method <- as.factor(gsub("DISP_|DIV_", replacement = "", x = lf$name))
-lf$Objective <- as.factor(
-  gsub("_E_1|_E_ALL|_VANILLA", replacement = "", 
-  x = lf$name)
-)
-
-lf$K <- factor(lf$K)
-lf$N <- factor(lf$N)
-lf$M <- factor(lf$M)
-lf$file <- factor(lf$file)
-
-lf_div <- subset(lf, Objective == "DIV" & VANILLA_FOUND_OPTIMUM)
-
-m1 <- lmBF(value ~ N + M + file, whichRandom = "file", data = lf_div)
-m2 <- lmBF(
-  value ~ N + M + Method + file, whichRandom = "file", 
-  data = lf_div
-)
-
-m2/ m1
-
-ttestBF(
-  lf_div$value[lf_div$Method == "VANILLA"],
-  lf_div$value[lf_div$Method == "E_ALL"],
-  paired = TRUE
-)
+# - RESTRICTED METHOD IS NOT GOOD IF THE NUMBER OF UNIQUE PARTITIONS IS SMALL 
